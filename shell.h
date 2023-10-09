7927eccffaf4669412b34c8a496d7bcfbeae5cc6
prevail98 <prevailoyedepo5@gmail.com>
2023-10-09 08:25:58 -0700

    pp

---
+++ shell.h
@@ -0,0 +1,8 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+
+int add (int num1,int num2);
+int main (void);
+
+
+#endif
---
+++ test.c
@@ -0,0 +1,17 @@
+#include "shell.h"
+#include <stdio.h>
+/**
+ * main - the main function
+ * Return: returns zero
+ */
+
+int main(void)
+{
+
+	int a = 4;
+	int b = 3;
+	int result = add(a, b);
+
+	printf("%d", result);
+	return (0);
+}
