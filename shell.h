99fa373402e2ef9b5d44c8d97f12d677aafc11fc
prevail98 <prevailoyedepo5@gmail.com>
2023-10-17 05:52:11 -0700

    tegsbmywifeijn

--- shell.h
+++ shell.h
@@ -11,5 +11,12 @@ int main(int ac, char **av, char **env);
 ssize_t _getline(char **lineptr, size_t *n, FILE *stream);
 char *_strcat(char *des, char *src);
 char **_strtok(char *str);
+void signal_handle(int signal);
+char *_remove(char **str);
+size_t _strlen(char *str);
+char *empty_string(char **str);
+char *_strdup(char **strd, char *ostr);
+char *_append(char *str, char *buffer);
+
 
 #endif
